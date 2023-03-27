0b2a517c3f7c8b0913ceefe0d6a0e1126e1ca810
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-03-27 12:50:01 +0200

    arrays+push+pop

--- js-tableaux/ex.js
+++ js-tableaux/ex.js
@@ -1,6 +1,13 @@
 //Simple
 let monTableau = ['un', 'deux','trois'];
 
+monTableau.push('quatre'); //Ajoute l'element quatre à la suite de mon tableau
+monTableau.unshift('zero'); // Ajoute l'element zero au debut de mon tableau
+monTableau.pop(); //Supprime le dernier element du tableau
+monTableau.shift(); //Supprimer le premier element du tableau
+
+/*Le fait d'ajouter/ supprimer des elements du tableaux modifie les indexs */
+
 console.log(monTableau[0]);
 console.log(monTableau.length); // nombre d'element dans mon tableau
 
@@ -9,6 +16,9 @@ let monTableau2D = [
     ['Mark', 'Jeff', 'Bill'],
     ['Zuckerberg', 'Bezos', 'Gates']
 ];
+
+monTableau2D[0].push('test'); // Ajoute l'element test au premier tableau
+/*Preciser à quel niveau on veut supprimer/ajouter pour les tableaux à plusieurs dimensions */
 
 console.log(monTableau2D[0][1]);
 
@@ -19,4 +29,5 @@ let monTableauAssociatif = {
     'poste'     : 'PDG de Facebook'
 };
 
+delete(monTableauAssociatif.poste); // L'element poste a été supprimé
 console.log(monTableauAssociatif['poste']);
