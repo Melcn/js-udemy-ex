b130be094212589c08271576d1e1a1eaa790bd12
Melanie C <121883970+Melcn@users.noreply.github.com>
2023-04-13 10:29:18 +0200

    dom+append+remove

--- DOM/johnDoe.js
+++ DOM/johnDoe.js
@@ -1,4 +1,4 @@
-// Les selecteurs
+// *** LES SELECTEURS ***
 
 let header  = document.getElementsByTagName('header');
 let logo    = document.getElementById('logo');
@@ -18,4 +18,31 @@ console.log(container);
 console.log(h1);
 console.log(p);
 
+// *** AJOUTER DES ELEMENTS AU DOM ***
 
+// Ajouter du texte à la suite 
+document.write('test');
+
+//Ajouter un element brut 
+h1.append('test');
+
+//Pour les objets
+//Creer un elemement
+let helloWorld = document.createElement('div');
+
+//le personnaliser
+helloWorld.textContent = "Hello World !";
+
+//L'ajouter à notre page
+//document.body.append(helloWorld);
+//Affiche mon objet Hello World avant container
+document.querySelector('.container').prepend(helloWorld);
+
+
+// *** SUPPRIMER DES ELEMENTS ***
+
+//remove en ayant stock dans une variable au préalable
+h1.remove();
+
+//remove en selectionnant directement
+document.querySelector('h1').remove();
